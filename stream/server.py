04b9dfc643971cbaf9d2bed779a604a232191b9c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-11 15:14:48 +0900

    live-time frame processing

--- stream/server.py
+++ stream/server.py
@@ -2,14 +2,27 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+from PIL import Image
+from io import BytesIO
+import base64
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @socketio.on('frame')
 def received_frame(url):
+    url = url.replace('data:image/png;base64,', '')
     print(datetime.now(), 'Frame Received!')
-    socketio.emit('processed', 'wow')
+
+    frame = Image.open(BytesIO(base64.b64decode(url)))
+    
+    frame = frame.convert('RGB') # get rid of alpha channel for JPEG format
+    buffered = BytesIO()
+    frame.save(buffered, format='JPEG')
+    processed_url = base64.b64encode(buffered.getvalue())
+
+    socketio.emit('processed', 'data:image/png;base64,' + str(processed_url.decode()))
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=8001)
--- zepeto.py
+++ zepeto.py
@@ -4,6 +4,7 @@ from PIL import Image
 API_HOST = '47.74.149.35/api'
 PHOTOBOOTH_ID = '14H8320f7MYeqkg0UEwmGY'  # PHOTOBOOTH_ONE_87
 HASHCODE = '1VREP0'
+# HASHCODE = 'MD92C6'
 URL = f'http://{API_HOST}/photo/{PHOTOBOOTH_ID}/?hashCodes={HASHCODE}&width=500'
 
 def main():
